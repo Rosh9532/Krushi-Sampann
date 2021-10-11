6cb3db566088770dd4c7eb2545658888cf964946
Roshni Chauhan <68471779+Rosh9532@users.noreply.github.com>
2021-10-11 16:22:52 +0530

    fix:remove bug

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const path = require('path');
+
 const env=require("dotenv");
 const cors = require('cors');
 
